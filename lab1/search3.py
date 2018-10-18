71e2cba979e807380d8ee80c951e46074036d661
klos <galeo09@gmail.com>
2018-10-18 23:14:38 +0200

    working with keywords

--- lab1/search3.py
+++ lab1/search3.py
@@ -16,7 +16,6 @@ stemmer = PorterStemmer()
 # stopWords = stopwords.words('english')
 count_cosine = True
 d = False
-k = False
 
 # options
 query_string = 'machine'
@@ -24,9 +23,7 @@ if len(sys.argv) == 2:
   if sys.argv[1][0] == '-':
     count_cosine = False
     print('options')
-    if sys.argv[1][1] == 'k':
-      print('show preprocessed keywords')
-    elif sys.argv[1][1] == 'd':
+    if sys.argv[1][1] == 'd':
       d = True
       print('show preprocessed documents')
   else:
@@ -45,43 +42,47 @@ with open('./documents/documents.txt', 'r', encoding='iso-8859-1') as documents_
     tmp = doc.replace('\n', ' ').replace('\n ', ' ').translate(str.maketrans("", "", string.punctuation)).lower()
     documents.append(tmp)
   
-  
+
 # KEYWORDS
-keywords = []
+keywords = ''
 with open('./documents/keywords.txt', 'r') as keywords_file:
   keywords = keywords_file.read().replace('\n', ' ').lower()
 
-#print(documents)
-
 def stem_tokens(tokens, stemmer):
-    stemmed = []
-    for item in tokens:
-        stemmed.append(stemmer.stem(item))
-    return stemmed
+  stemmed = []
+  for item in tokens:
+    stemmed.append(stemmer.stem(item))
+  return stemmed
 
 def tokenize(text):
-    tokens = nltk.word_tokenize(text)
-    stems = stem_tokens(tokens, stemmer)
-    return stems
+  tokens = nltk.word_tokenize(text)
+  stems = stem_tokens(tokens, stemmer)
+  return stems
 
-i = 0
-for document in documents:
-  token_dict[i] = document
-  i += 1
+def delete_multiple_occ(collection):
+  for element in collection:
+    if collection.count(element) > 1:
+      print(element)
+      collection.remove(element)
 
+t_keywords = tokenize(keywords)
+
+delete_multiple_occ(t_keywords)
+
+print(len(t_keywords))
 
 #this can take some time 
 if count_cosine:
-  tfidf = TfidfVectorizer(tokenizer=tokenize, stop_words=None)
-  tfs = tfidf.fit_transform(token_dict.values())
+  tfidf = TfidfVectorizer(tokenizer=tokenize, stop_words=None, vocabulary=t_keywords)
+  tfs = tfidf.fit_transform(documents)
 
   feature_names = tfidf.get_feature_names()
 
   # similarity_matrix = cosine_similarity(tfs)
   print('Asked querry:\t"' + query_string + '"')
-  queruTFIDF = tfidf.transform([query_string])
+  queryTFIDF = tfidf.transform([query_string])
 
-  cosine = cosine_similarity(queruTFIDF, tfs).flatten()
+  cosine = cosine_similarity(queryTFIDF, tfs).flatten()
   related_docs = cosine.argsort()[:-11:-1]
 
   for r_doc in related_docs:
